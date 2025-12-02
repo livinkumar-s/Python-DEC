eb54a3d8d6d4ce9f38fbf9fa2dcbc370afbc1b91
LivinkumarStackly <livinkumar@thestackly.com>
2025-12-02 11:55:54 +0530

    Introduction

---
+++ introduction.py
@@ -0,0 +1,8 @@
+#the below lines are to print something
+'''The print() function in Python is used to output values to the screen or any other standard output device. It can handle various types of data, including strings, numbers, and other objects, converting them to strings before printing.
+
+Basic Usage
+'''
+print(200) 
+print(300)
+print(400)
