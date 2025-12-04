dd628697074f4eb346dd1835149e56c6e6f4adaf
LivinkumarStackly <livinkumar@thestackly.com>
2025-12-04 11:44:16 +0530

    Variables, Datatypes and Type casting

---
+++ VarDatatypeCasting.py
@@ -0,0 +1,36 @@
+a=10
+num1=45
+# 1num=78
+b="Hello world"
+c=99.199
+d=True 
+e=False
+
+# print(type(e)) #<class 'str'>
+
+# print("Hello"+"World"+"...!")
+# print("100"+"99"+"98")
+
+# print("hello"+99)
+
+# name="Prabas"
+# age=45
+
+# print("His name is " + name +". He is "+str(age)+" years old.")
+ 
+
+# His name is kamal. He is 70 years old.
+
+# a=10
+# print(type(str(a)))
+
+# a=input("Enter your name: ")
+
+# print(type(a))
+# print(a)
+# print(a)
+# print(a)
+# print(a)
+
+num=int(input("Enter a number: "))
+print(100+num)
