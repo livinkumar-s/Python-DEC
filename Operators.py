98ece0f01481a974e2a430c83e38e737040dd91e
LivinkumarStackly <livinkumar@thestackly.com>
2025-12-05 11:49:09 +0530

    Operators

---
+++ Operators.py
@@ -0,0 +1,58 @@
+a=90
+b=89
+
+# print(a/b)
+# print(a//b)
+# print(78//30)
+# print(99//158)
+
+# print(3%4)
+# print(2**6)
+
+# print(6+8-7+4*8/2)
+
+# Comparison 
+
+# a=-10
+# b=-11+1
+
+# print(a==b)
+# print(a!=b)
+# print(a<b)
+# print(b>a)
+# print(a<=b)
+
+# Logical
+
+# a=False
+# b=False
+
+# print(a and b)
+# print(a or b)
+# print(not not a)
+
+# a=10
+# b=99
+# print((a>=b) and (a**b>a**(4*2) or a*8>a**2 and not(a<=b-(3**2))))
+
+# Assignment
+
+# a=10
+# a+=50
+# a-=35
+# a*=4
+# a/=10
+# a//=3
+# a%=10
+# a**=3
+
+# print(a)
+
+# Bitwise
+# print(8&20)
+# print(8|20)
+
+# print(14<<3)
+# print(14>>3)
+
+# print(~20)
