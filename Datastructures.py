93e10d06d950659846b36ddf58d13b5fee8ccb11
LivinkumarStackly <livinkumar@thestackly.com>
2025-12-11 11:59:30 +0530

    Datastructures - List

---
+++ Datastructures.py
@@ -0,0 +1,44 @@
+# a="Hello world"
+# a[4]='h'
+
+# print(a[-3])
+
+# print(a[-4:])
+# print(a[:6]) --> 0,1,2,3,4,5
+# print(a[2:9]) --> 2,3,4,5,6,7,8
+# print(a[::-1])
+
+# List 
+
+# a=[1,2,-3,-4,"five","six","seven",True,False]
+
+# print(a[:6]) #-->0,1,2,3,4,5
+# print(a[2:]) #--> 2,3,....final
+
+# a[4]=5
+
+# print(a)
+
+
+# lis1.append(100)
+# lis1.append(101)
+
+# lis1.extend(range(51))
+# lis1.insert(4,"Fuzz")
+
+# lis1.pop(3)
+# lis1.pop()
+# lis1.pop()
+# lis1.pop()
+
+lis1=[43,546,43,56,4,76,45,87,8,87,99]
+
+# lis1.remove(87)
+# lis1.sort()
+# lis1.reverse()
+
+# print(lis1)
+
+# print(lis1.count(7))
+
+# print(lis1.index(4))
