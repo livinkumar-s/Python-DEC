3f8df607ebb4c0756913d4b27fd951810839bb58
LivinkumarStackly <livinkumar@thestackly.com>
2025-12-15 11:54:38 +0530

    Functions

---
+++ Functions.py
@@ -0,0 +1,54 @@
+# def printSteps():
+#     print("Step1")
+#     print("Step2")
+#     print("Step3")
+
+# printSteps()
+# printSteps()
+
+# def addVal(a,b):
+#     print(a+b)
+
+# addVal(8,9)
+# addVal(90,80)
+
+# print(10)
+
+# def greaterOne(a,b):
+#     if a>b:
+#         print("a")
+#     else:
+#         print("b")
+
+# greaterOne(100,101)
+# greaterOne(b=70,a=100)
+
+
+# def mulVal(a,b=20):
+#     print(a*b)
+
+# mulVal(10,60)
+
+# def steps():
+#     print("Started")
+#     print("Step1")
+#     print("Step2")
+#     print("Step3")
+#     return 67
+#     print("Step4")
+#     print("Step5")
+#     print("Final Step")
+
+
+# print(steps())
+
+# def multiply(a,b):
+#     return a*b
+
+
+# print(432)
+
+# a=input("Enter a number: ")
+# print(a)
+
+# check ---> num ---> odd, even 
