b0ac6e31336e4d7235b18ed0837615e8bdf7d095
LivinkumarStackly <livinkumar@thestackly.com>
2025-12-16 12:09:53 +0530

    String

---
+++ Strring.py
@@ -0,0 +1,46 @@
+a="    his name is john.       "
+# print(a[2:6]) #2,3,4,5
+# print(a*26)
+
+# print('vi' not in a)
+
+# print(a.upper())
+# print(a.lower())
+# print(a.title())
+# print(a.capitalize())
+# print(a.strip())
+
+b="Hello guys...Hello Baiya... Hello buddies"
+
+# print(b.replace("Hello","Vanakkam"))
+
+c="I am livinkumar. From Tamilnadu."
+
+
+# print(c.count("a"))
+# print(c.find("arr"))
+
+#[]
+
+d=['I', 'am', 'livinkumar.', 'From', 'Tamilnadu.']
+
+# print("----".join(d))
+
+# e="123e"
+
+# print(e.isalpha())
+# print(e.isdigit())
+# print(e.isalnum())
+
+
+# name="Vikram"
+# age=53
+
+# print("His name is "+name+". He is "+str(age)+".")
+# print(f"His name is {name}. He is {age}.")
+
+#His name is Vikram. He is 53.
+
+a="Hello"
+b=a+"Guys!"
+print(b)
